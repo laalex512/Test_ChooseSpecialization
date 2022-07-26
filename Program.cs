a383ec211cba6eb2ef6f84b35167055d3247e0b9
laalex <laalex512@gmail.com>
2022-07-26 20:34:14 +0300

    Add Method FindElements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] FindElements(string[] inputArray, int countSymbol)
+{
+    int countOutput = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= countSymbol)
+            countOutput++;
+    }
+    string[] outputArray = new string[countOutput];
+    int indexOutput = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= countSymbol)
+        {
+            outputArray[indexOutput] = inputArray[i];
+            indexOutput++;
+        }
+    }
+    return outputArray;
+}
+
